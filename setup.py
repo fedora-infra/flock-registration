1f59eaf0975bc7c4ebbd5766f02a0a5cc86cad8d
Luke Macken <lmacken@redhat.com>
2013-04-22 12:10:44 -0400

    Update the setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='fudcon-registration',
+    name='flock-registration',
     version='0.1',
-    description='A Flask-based application for handling FUDCon registrations',
-    author='Ian Weller',
+    license='GPLv2+',
+    description='A Flask-based application for handling Fedora Flock registrations',
+    author='Ian Weller, Luke Macken',
     author_email='ianweller@fedoraproject.org',
     url='http://fedoraproject.org/wiki/FUDCon',
     install_requires=['Flask', 'Flask-OpenID', 'Flask-PyMongo', 'Flask-WTF', 'bunch'],
