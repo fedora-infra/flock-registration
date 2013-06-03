45e560d3df9ff7b56bffc110cac9f4105d681189
Luke Macken <lmacken@redhat.com>
2013-06-02 22:46:24 -0400

    Sort proposals by creation date to fix broken links in export

--- scripts/proposals2voting.py
+++ scripts/proposals2voting.py
@@ -6,14 +6,19 @@ import json
 baseurl = 'http://register.flocktofedora.org'
 proposals = []
 
+data = []
+
 with file('proposals.json') as f:
-    for i, row in enumerate(f):
+    for row in f:
         row = json.loads(row)
-        if '!' in row['title']:
-            row['title'] = row['title'].replace('!', u'\u203C')
-        assert '!' not in row['title'], 'Title contains "!": %r' % row['title']
-        assert '|' not in row['title'], 'Title contains "|"'
-        url = baseurl + '/proposals#%d' % (i + 1)
-        proposals.append('%s!%s' % (row['title'], url))
+        data.append(row)
+
+for i, row in enumerate(sorted(data, key=lambda x: x['created']['$date'])):
+    if '!' in row['title']:
+        row['title'] = row['title'].replace('!', u'\u203C')
+    assert '!' not in row['title'], 'Title contains "!": %r' % row['title']
+    assert '|' not in row['title'], 'Title contains "|"'
+    url = baseurl + '/proposals#%d' % (i + 1)
+    proposals.append('%s!%s' % (row['title'], url))
 
 print('|'.join(proposals))
